7737f05e3e3f2e1d210d62d7d0bb3c2b15ce3be0
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-10-15 19:51:46 +0700

    chore: Register Category/Product in Admin

--- app/store/admin.py
+++ app/store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    empty_value_display = '---'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ordering = ('pk',)
+    list_filter = ('status',)
+    list_display = ('name', 'slug', 'category', 'price', 'status')
+    list_editable = ('slug', 'price')
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'updated_at')
+    raw_id_fields = ('category',)
+    save_as = False
