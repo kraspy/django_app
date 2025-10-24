108939ea646ab6c41ade2b6062468dc1ccb686bc
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-10-24 18:17:18 +0700

    chore: Add basic templates

--- app/store/urls.py
+++ app/store/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
+from . import views
+
+app_name = 'store'
+
 urlpatterns = [
-    path(
-        '',
-    )
+    path('', views.page_index, name='index'),
+    path('products/', views.products, name='products'),
+    path('products/<int:pk>/', views.product, name='product'),
 ]
--- app/store/views.py
+++ app/store/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Category, Product
+
 
 # Create your views here.
+def page_index(request):
+    return render(request, 'store/index.html')
+
+
+def products(request):
+    context = {
+        'products': Product.objects.all(),
+    }
+
+    return render(request, 'store/products.html', context)
+
+
+def product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'store/product.html', context)
