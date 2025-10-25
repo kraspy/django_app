827db0161639f9e3d684d0d147c4e3da7ed4dd88
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-10-25 14:05:56 +0700

    chore: Create admin actions

--- app/store/admin.py
+++ app/store/admin.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.contrib import admin
 
 from store.models import Category, Product
@@ -20,3 +22,34 @@ class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at')
     raw_id_fields = ('category',)
     save_as = False
+
+    @admin.action(description='Обнулить цены')
+    def set_zero_prices(self, request, queryset: list[Product]):
+        for product in queryset:
+            product.price = 0
+            product.save()
+
+    @admin.action(description='Установить рандомные цены')
+    def set_random_prices(self, request, queryset: list[Product]):
+        for product in queryset:
+            product.price = randint(100, 1000)
+            product.save()
+
+    @admin.action(description='Опубликовать')
+    def set_published(self, request, queryset: list[Product]):
+        for product in queryset:
+            product.status = Product.Status.ACTIVE
+            product.save()
+
+    @admin.action(description='Снять с публикации')
+    def set_unpublished(self, request, queryset: list[Product]):
+        for product in queryset:
+            product.status = Product.Status.INACTIVE
+            product.save()
+
+    actions = (
+        set_zero_prices,
+        set_random_prices,
+        set_published,
+        set_unpublished,
+    )
