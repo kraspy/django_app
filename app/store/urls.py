797ead3eae5c7fc461dd14126fe2129f6ff837f3
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-11-05 14:44:52 +0700

    rewrite FBV to CBV

--- app/store/urls.py
+++ app/store/urls.py
@@ -5,14 +5,38 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.page_index, name='index'),
-    path('products/', views.products, name='products'),
-    path('products/<int:pk>/', views.product, name='product'),
-    path('products/add/', views.add_product, name='add_product'),
-    path('products/edit/<int:pk>/', views.edit_product, name='edit_product'),
+    path(
+        '',
+        views.IndexPageTemplateView.as_view(),
+        name='index',
+    ),
+    path(
+        'products/',
+        views.ProductsListView.as_view(),
+        name='products',
+    ),
+    path(
+        'products/<int:pk>/',
+        views.ProductDetailView.as_view(),
+        name='product',
+    ),
+    path(
+        'products/add/',
+        views.AddProductCreateView.as_view(),
+        name='add_product',
+    ),
+    path(
+        'products/edit/<int:pk>/',
+        views.EditProductUpdateView.as_view(),
+        name='edit_product',
+    ),
     path(
         'products/delete/<int:pk>/',
-        views.remove_product,
+        views.ProductDeleteView.as_view(),
         name='remove_product',
+    ),
+    path(
+        'qwerty',
+        views.MyView.as_view(),
     ),
 ]
--- app/store/views.py
+++ app/store/views.py
@@ -1,88 +1,142 @@
+from django import views
 from django.core.paginator import Paginator
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
 
 from .forms import AddProductForm, RemoveProductForm
 from .models import Category, Product
 
-
-# Create your views here.
-def page_index(request):
-    return render(request, 'store/index.html')
-
-
-def products(request):
-    products = Product.objects.all()
-    paginator = Paginator(products, 8)
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    context = {
-        'page_obj': page_obj,
-    }
-
-    return render(request, 'store/products.html', context)
+# # Create your views here.
+# def page_index(request):
+#     return render(request, 'store/index.html')
 
 
-def product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'store/product.html', context)
+class IndexPageTemplateView(TemplateView):
+    template_name = 'store/index.html'
 
 
-def add_product(request):
-    if request.method == 'POST':
-        form = AddProductForm(request.POST)
+# def products(request):
+#     products = Product.objects.all()
+#     paginator = Paginator(products, 8)
 
-        if form.is_valid():
-            form.save()
-            return redirect('store:index')
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
 
-    else:
-        form = AddProductForm()
+#     context = {
+#         'page_obj': page_obj,
+#     }
 
-    return render(request, 'store/add_product.html', {'form': form})
-
-
-def edit_product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-
-    if request.method == 'POST':
-        form = AddProductForm(instance=product)
-
-        if form.is_valid():
-            form.save()
-            return redirect('store:product', pk=pk)
-
-    else:
-        form = AddProductForm(instance=product)
-
-    context = {
-        'form': form,
-        'product': product,
-    }
-
-    return render(request, 'store/edit_product.html', context)
+#     return render(request, 'store/products.html', context)
 
 
-def remove_product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+class ProductsListView(ListView):
+    model = Product
+    paginate_by = 8
+    template_name = 'store/products.html'
 
-    if request.method == 'POST':
-        form = RemoveProductForm(request.POST)
-        if form.is_valid():
-            product.delete()
-            return redirect('store:products')
-    else:
-        form = RemoveProductForm()
 
-    context = {
-        'form': form,
-        'product': product,
-    }
+# def product(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
 
-    return render(request, 'store/remove_product.html', context)
+#     context = {
+#         'product': product,
+#     }
+
+#     return render(request, 'store/product.html', context)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'store/product.html'
+
+
+# def add_product(request):
+#     if request.method == 'POST':
+#         form = AddProductForm(request.POST)
+
+#         if form.is_valid():
+#             form.save()
+#             return redirect('store:index')
+
+#     else:
+#         form = AddProductForm()
+
+#     return render(request, 'store/add_product.html', {'form': form})
+
+
+class AddProductCreateView(CreateView):
+    model = Product
+    template_name = 'store/add_product.html'
+    form_class = AddProductForm
+    success_url = reverse_lazy('store:products')
+
+
+# def edit_product(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+
+#     if request.method == 'POST':
+#         form = AddProductForm(instance=product)
+
+#         if form.is_valid():
+#             form.save()
+#             return redirect('store:product', pk=pk)
+
+#     else:
+#         form = AddProductForm(instance=product)
+
+#     context = {
+#         'form': form,
+#         'product': product,
+#     }
+
+#     return render(request, 'store/edit_product.html', context)
+
+
+class EditProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'store/edit_product.html'
+    form_class = AddProductForm
+    success_url = reverse_lazy('store:product')
+
+    def get_success_url(self):
+        return reverse_lazy('store:product', kwargs={'pk': self.object.pk})
+
+
+# def remove_product(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+
+#     if request.method == 'POST':
+#         form = RemoveProductForm(request.POST)
+#         if form.is_valid():
+#             product.delete()
+#             return redirect('store:products')
+#     else:
+#         form = RemoveProductForm()
+
+#     context = {
+#         'form': form,
+#         'product': product,
+#     }
+
+#     return render(request, 'store/remove_product.html', context)
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'store/remove_product.html'
+    success_url = reverse_lazy('store:products')
+
+
+class MyView(View):
+    def get(self, request):
+        return HttpResponse(request)
